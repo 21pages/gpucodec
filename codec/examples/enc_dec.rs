53d56e39a199cc10c6654d1166b1aa17f259a5ed
21pages <pages21@163.com>
2023-03-23 17:12:55 +0800

    codec: change example enc_dec encode para
    Signed-off-by: 21pages <pages21@163.com>

--- codec/examples/enc_dec.rs
+++ codec/examples/enc_dec.rs
@@ -38,37 +38,36 @@ fn main() {
     let mut encoded_file = std::fs::File::create(encoded_file_name).unwrap();
     let mut decoded_file = std::fs::File::create(decoded_file_name).unwrap();
 
-    let len = 7776000;
-    let mut buf = vec![0u8; len];
+    let yuv_len = (en_ctx.width * en_ctx.height * 3 / 2) as usize;
+    let mut yuv = vec![0u8; yuv_len];
     let mut enc_sum = Duration::ZERO;
     let mut dec_sum = Duration::ZERO;
     let mut enc_counter = 0;
     let mut dec_counter = 0;
-    unsafe {
-        for _ in 0..100 {
-            yuv_file.read(&mut buf).unwrap();
+    for _ in 0..100 {
+        yuv_file.read(&mut yuv).unwrap();
+        let start = Instant::now();
+        let linesizes: Vec<i32> = vec![en_ctx.width, en_ctx.width];
+        let ysize = (linesizes[0] * en_ctx.height) as usize;
+        let y = &yuv[0..ysize];
+        let uv = &yuv[ysize..];
+        let datas = vec![y, uv];
+        let data = encoder.encode(datas, linesizes).unwrap();
+        enc_sum += start.elapsed();
+        enc_counter += data.len();
+        for f in data {
+            encoded_file.write_all(&f.data).unwrap();
             let start = Instant::now();
-            let linesizes: Vec<i32> = vec![2880, 2880];
-            let y = buf.as_ptr();
-            let uv = y.add((linesizes[0] * en_ctx.height) as _);
-            let datas = vec![y, uv];
-            let data = encoder.encode(datas, linesizes).unwrap();
-            enc_sum += start.elapsed();
-            enc_counter += data.len();
-            for f in data {
-                encoded_file.write_all(&f.data).unwrap();
-                let start = Instant::now();
-                let decoded = decoder.decode(&f.data).unwrap();
-                dec_sum += start.elapsed();
-                dec_counter += 1;
-                for f in decoded {
-                    for v in f.data.iter() {
-                        decoded_file.write_all(v).unwrap();
-                    }
+            let decoded = decoder.decode(&f.data).unwrap();
+            dec_sum += start.elapsed();
+            dec_counter += 1;
+            for f in decoded {
+                for v in f.data.iter() {
+                    decoded_file.write_all(v).unwrap();
                 }
             }
         }
-        println!("enc:avg:{:?}, counter:{}", enc_sum / 100, enc_counter);
-        println!("dec:avg:{:?}, counter:{}", dec_sum / 100, dec_counter);
     }
+    println!("enc:avg:{:?}, counter:{}", enc_sum / 100, enc_counter);
+    println!("dec:avg:{:?}, counter:{}", dec_sum / 100, dec_counter);
 }
