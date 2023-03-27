1c8f5f793462ce185863893c7afc50bbf10e7599
21pages <pages21@163.com>
2023-03-27 08:50:50 +0800

    rename mux to ffmpeg
    Signed-off-by: 21pages <pages21@163.com>

--- ffmpeg/examples/mux.rs
+++ ffmpeg/examples/mux.rs
@@ -1,7 +1,7 @@
 use codec_common::{DataFormat, EncodeContext, EncodeDriver, HWDeviceType, PixelFormat};
 use env_logger::{init_from_env, Env, DEFAULT_FILTER_ENV};
+use ffmpeg::mux::{MuxContext, Muxer};
 use hwcodec::encode::Encoder;
-use mux::mux::{MuxContext, Muxer};
 use std::{io::Read, path::PathBuf, time::Instant};
 
 fn main() {
