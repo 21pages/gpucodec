10549a89e6c723fa2152458785efbd84e1d28abb
21pages <pages21@163.com>
2023-04-03 15:04:48 +0800

    move codec to hwcodec for dir name same with crate name
    Signed-off-by: 21pages <pages21@163.com>

--- codec/src/ffi.h
+++
@@ -1,8 +0,0 @@
-#ifndef FFI_H
-#define FFI_H
-
-#include <stdint.h>
-
-void get_bin_file(int32_t is265, uint8_t **p, int32_t *len);
-
-#endif
--- codec/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-include!(concat!(env!("OUT_DIR"), "/codec_ffi.rs"));
-
-pub mod decode;
-pub mod encode;
