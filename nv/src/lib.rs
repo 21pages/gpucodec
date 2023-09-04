462a3aee02a3ae0b484a370a2e91ffb303963f38
21pages <pages21@163.com>
2023-09-04 11:43:59 +0800

    enable nv decode
    Signed-off-by: 21pages <pages21@163.com>

--- nv/src/lib.rs
+++ nv/src/lib.rs
@@ -49,20 +49,19 @@ pub fn possible_support_encoders() -> Vec<InnerEncodeContext> {
 }
 
 pub fn possible_support_decoders() -> Vec<InnerDecodeContext> {
-    vec![]
-    // if unsafe { nv_encode_driver_support() } != 0 {
-    //     return vec![];
-    // }
-    // let devices = vec![API_DX11];
-    // let dataFormats = vec![H264, H265];
-    // let mut v = vec![];
-    // for device in devices.iter() {
-    //     for dataFormat in dataFormats.iter() {
-    //         v.push(InnerDecodeContext {
-    //             api: device.clone(),
-    //             dataFormat: dataFormat.clone(),
-    //         });
-    //     }
-    // }
-    // v
+    if unsafe { nv_encode_driver_support() } != 0 {
+        return vec![];
+    }
+    let devices = vec![API_DX11];
+    let dataFormats = vec![H264, H265];
+    let mut v = vec![];
+    for device in devices.iter() {
+        for dataFormat in dataFormats.iter() {
+            v.push(InnerDecodeContext {
+                api: device.clone(),
+                dataFormat: dataFormat.clone(),
+            });
+        }
+    }
+    v
 }
