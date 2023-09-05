8ff09800d0274d792a7d0b8ea24c83a8ab6e1fbe
21pages <pages21@163.com>
2023-09-05 16:45:53 +0800

    cuvid: change shader to color space 601
    Signed-off-by: 21pages <pages21@163.com>

--- nv/src/decode.cpp
+++ nv/src/decode.cpp
@@ -410,6 +410,7 @@ static bool create_sample(CuvidDecoder *p) {
 }
 
 static bool create_shader(CuvidDecoder *p) {
+  // https://gist.github.com/RomiTT/9c05d36fe339b899793a3252297a5624
   const char *vertexShaderCode = R"(
 struct VS_INPUT
 {
@@ -437,15 +438,16 @@ struct PS_INPUT
     float4 Pos : SV_POSITION;
     float2 Tex : TEXCOORD0;
 };
-
+// 601
 float4 PS(PS_INPUT input) : SV_TARGET{
   float y = g_txFrame0.Sample(g_Sam, input.Tex).r;
+  y = 1.164383561643836 * (y - 0.0625);
   float2 uv = g_txFrame1.Sample(g_Sam, input.Tex).rg - float2(0.5f, 0.5f);
   float u = uv.x;
   float v = uv.y;
-  float r = y + 1.14f * v;
-  float g = y - 0.394f * u - 0.581f * v;
-  float b = y + 2.03f * u;
+  float r = saturate(y + 1.596026785714286 * v);
+  float g = saturate(y - 0.812967647237771 * v - 0.391762290094914 * u);
+  float b = saturate(y + 2.017232142857142 * u);
   return float4(r, g, b, 1.0f);
 }
 )";
