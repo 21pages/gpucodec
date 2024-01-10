7e533e40bc9bb33350969f3e232623cbeac4616c
21pages <pages21@163.com>
2024-01-10 13:26:09 +0800

    vs can, but cargo run can't
    Signed-off-by: 21pages <pages21@163.com>

--- dev/vs/MFXTest/main.cpp
+++ dev/vs/MFXTest/main.cpp
@@ -13,17 +13,17 @@ int dxgi_height(const void *self);
 void *dxgi_capture(void *self, int wait_ms);
 void destroy_dxgi_capturer(void *self);
 
-void *mfx_new_encoder(void *hdl, int64_t luid, API api, DataFormat dataFormat,
+void *vpl_new_encoder(void *hdl, int64_t luid, API api, DataFormat dataFormat,
                       int32_t width, int32_t height, int32_t kbs,
                       int32_t framerate, int32_t gop);
-int mfx_encode(void *e, void *tex, EncodeCallback callback, void *obj);
-int mfx_destroy_encoder(void *e);
+int vpl_encode(void *e, void *tex, EncodeCallback callback, void *obj);
+int vpl_destroy_encoder(void *e);
 
-void *mfx_new_decoder(void *device, int64_t luid, int32_t api,
+void *vpl_new_decoder(void *device, int64_t luid, int32_t api,
                       int32_t dataFormat, bool outputSharedHandle);
-int mfx_decode(void *decoder, uint8_t *data, int32_t length,
+int vpl_decode(void *decoder, uint8_t *data, int32_t length,
                DecodeCallback callback, void *obj);
-int mfx_destroy_decoder(void *decoder);
+int vpl_destroy_decoder(void *decoder);
 
 void *CreateDXGIRender(long long luid, bool inputSharedHandle);
 int DXGIRenderTexture(void *render, HANDLE shared_handle);
@@ -66,7 +66,7 @@ int main() {
   int height = dxgi_height(dup);
   std::cout << "width: " << width << " height: " << height << std::endl;
   void *device = dxgi_device(dup);
-  void *encoder = mfx_new_encoder(device, luid, API_DX11, dataFormat, width,
+  void *encoder = vpl_new_encoder(device, luid, API_DX11, dataFormat, width,
                                   height, 4000, 30, 0xFFFF);
   if (!encoder) {
     std::cerr << "create encoder failed" << std::endl;
@@ -80,7 +80,7 @@ int main() {
   }
 
   void *decoder =
-      mfx_new_decoder(DXGIDevice(render), luid, API_DX11, dataFormat, false);
+      vpl_new_decoder(DXGIDevice(render), luid, API_DX11, dataFormat, false);
   if (!decoder) {
     std::cerr << "create decoder failed" << std::endl;
     return -1;
@@ -92,11 +92,11 @@ int main() {
       std::cerr << "texture is NULL" << std::endl;
       continue;
     }
-    if (0 != mfx_encode(encoder, texture, encode_callback, NULL)) {
+    if (0 != vpl_encode(encoder, texture, encode_callback, NULL)) {
       std::cerr << "encode failed" << std::endl;
       continue;
     }
-    if (0 != mfx_decode(decoder, (uint8_t *)encode_data, encode_len,
+    if (0 != vpl_decode(decoder, (uint8_t *)encode_data, encode_len,
                         decode_callback, NULL)) {
       std::cerr << "decode failed" << std::endl;
       continue;
